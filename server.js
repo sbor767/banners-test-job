5647ce8ce38927abbf56b933dc3e29b894df8693
Aleksandr Borkov <sbor767@yandex.ru>
2019-03-06 16:33:58 +0300

    Add fingerprint2 library to gets webgl hash and implemented it using with other data required by specification.

--- server.js
+++ server.js
@@ -8,17 +8,16 @@ const http = require('http')
 const url = require('url');
 const staticServer = require('node-static')
 
+// Gets random integer within 0..65535
+const getRandomInt = () =>  Math.floor(Math.random() * 2 ** 16)
 
 const fileServer = new staticServer.Server('./public')
 
 const server = http.createServer((req, res) => {
 
   req.addListener('end', () => {
-
     res.setHeader('Access-Control-Allow-Origin', '*')
-
     fileServer.serve(req, res, async (err, result) => {
-
       const errorEnd = (code, logMessage) => {
         console.error(logMessage)
         // Respond to the client
@@ -28,18 +27,30 @@ const server = http.createServer((req, res) => {
 
 
       if (err) {
+        /**
+         * Treat non file requests
+         */
 
-        // Treat non file requests
-
-        /* /get path */
+        /**
+         * path = '/get'
+         */
         const urlParsed = url.parse(req.url, true)
 
         if (urlParsed.pathname === '/get') {
 
           try {
+            const randomClickId = getRandomInt()
             const banner = await require('./api/banners').getRandom()
-            const html = `<a href="${banner.href}" target="_blank"><img src="${protocol}://${hostname}${portStr}/banners/${banner.fileName}" alt="${banner.title}" width="${banner.width}" height="${banner.height}" /></a>`
-            res.end(html)
+            const html =
+              `<a href="${banner.href}" target="_blank">` +
+                `<img ` +
+                  `src="${protocol}://${hostname}${portStr}/banners/${banner.fileName}?click_id=${randomClickId}" ` +
+                  `alt="${banner.title}" ` +
+                  `width="${banner.width}" ` +
+                  `height="${banner.height}" ` +
+                `/>` +
+              `</a>`
+            res.end(JSON.stringify({randomId: randomClickId, html}))
             return
 
           } catch (e) {
@@ -57,19 +68,45 @@ const server = http.createServer((req, res) => {
 })
 
 
+/**
+ * Run server
+ */
 server.listen(port, hostname, () => {
-  // Add .env variables and form script.js from template.
+
+  // @TODO Move next to webpack, but for now we do this by pure node.
+  // Prepare build-up client script.js by adding fingerprint library, environment variables and basic script.js
+  // from template folder.
   const fs = require('fs')
-  const readable = fs.createReadStream(__dirname + '/templates/client/script.js', { encoding: 'utf8', highWaterMark: 16 * 1024 })
+  const fingerprintLibrary = fs.createReadStream(__dirname + '/templates/libraries/fingerprint2.js', { encoding: 'utf8', highWaterMark: 64 * 1024 })
+  const sourceScript = fs.createReadStream(__dirname + '/templates/client/script.js', { encoding: 'utf8', highWaterMark: 16 * 1024 })
   const writable = fs.createWriteStream(__dirname + '/public/client/script.js')
 
+  fingerprintLibrary.pipe(writable, { end: false })
+
+  // Creating readable stream from string (https://stackoverflow.com/a/22085851)
+  const Readable = require('stream').Readable
+  const s = new Readable()
+  s._read = () => {} // redundant? see update below
+  // Insert for code separation.
+  s.push('\n\n')
   // Insert current .env vars.
   const envConstants =
     `const protocol = '${protocol}'\n` +
     `const hostname = '${hostname}'\n` +
-    `const portStr = '${portStr}'\n\n`
-  writable.write(envConstants)
-  readable.pipe(writable)
+    `const portStr = '${portStr}';/**/\n\n`
+  s.push(envConstants)
+  // Insert to denote the end of stream.
+  s.push(null)
+
+  // Writing two readable streams into the same writable stream.
+  // See https://stackoverflow.com/a/28033554.
+  fingerprintLibrary.on('end', () => {
+    s.pipe(writable, { end: false })
+  })
+
+  s.on('end', () => {
+    sourceScript.pipe(writable)
+  })
 
   console.log(`Server running at http://${hostname}${portStr}/`)
 })
--- templates/client/script.js
+++ templates/client/script.js
@@ -1,27 +1,164 @@
 ;(function() {
 
-  const uri = `${protocol}://${hostname}${portStr}/get?id=97`
+  // const
+  const refDomen = document.domain
+  console.log('refDomen=', refDomen)
+  const referrer = document.referrer
+  console.log('referrer=', referrer)
+  const location = document.location.href
+  console.log('location=', location)
 
-  fetch(uri)
+  const date = new Date()
+  const timezoneStr = date.toString().match(/\((.*)\)/).pop()
+  console.log('timezoneStr=', timezoneStr)
+  const timezoneOffset = date.getTimezoneOffset()
+  console.log('timezoneOffset=', timezoneOffset)
 
-    .then(response => {
+  const plugins = () => {
+    const plugins = navigator.plugins
+    let pluginsStr = ''
+    for (let i = 0; i < plugins.length; i++) {
+      pluginsStr += pluginsStr ? ',' + plugins[i].name : plugins[i].name
+    }
+    return pluginsStr
+  }
+  console.log('plugins=', plugins())
 
-      if(response.ok) {
-        return response.text()
+  const hasCookie = !!document.cookie
+  console.log('hasCookie=', hasCookie)
+
+  // It is also possible to get a lot of what is higher in the code using the following:
+
+
+  /**
+   * Returns Promise
+   */
+  const fingerprintHashDo = () => {
+    /**
+     * WebGl hash
+     * API from https://github.com/Valve/fingerprintjs2
+     * What is - https://browserleaks.com/webgl#what-is-webgl-fingerprinting
+     * Also possible approach and code see on https://codepen.io/jon/pen/LLPKbz
+     */
+    const options = {
+      excludes: {
+        userAgent: true,
+        webdriver: true,
+        language: true,
+        colorDepth: true,
+        deviceMemory: true,
+        pixelRatio: true,
+        hardwareConcurrency: true,
+        screenResolution: true,
+        availableScreenResolution: true,
+        timezoneOffset: true,
+        timezone: true,
+        sessionStorage: true,
+        localStorage: true,
+        indexedDb: true,
+        openDatabase: true,
+        cpuClass: true,
+        platform: true,
+        doNotTrack: true,
+        plugins: true,
+        canvas: true,
+        adBlock: true,
+        hasLiedLanguages: true,
+        hasLiedResolution: true,
+        hasLiedBrowser: true,
+        touchSupport: true,
+        fonts: true,
+        audio: true,
+        enumerateDevices: true,
+
+        addBehavior: true,
+        hasLiedOs: true,
+        fontsFlash: true,
+
+        webglVendorAndRenderer: true
       }
-      throw new Error('Network response was not ok.')
-    })
+    }
+    return Fingerprint2.getPromise(options)
+      .then(components => { // an array of components: {key: ..., value: ...}
+        console.log('Fingerprint2.getPromise--result=', components)
+        return Fingerprint2.x64hash128(components.join(''), 31)
+      })
+      .then(hash => {
+        console.log('Fingerprint2.getPromise--x64hash128=', hash)
+        return hash
+      })
+  }
 
-    .then(text => {
+  /**
+   * Returns Promise
+   */
+  const loadFingerprint2Cdn = () => {
+    const cdn = 'https://cdnjs.cloudflare.com/ajax/libs/fingerprintjs2/2.0.6/fingerprint2.js'
+    fetch(cdn)
+      .then(response => {
+        if(response.ok) {
+          return response.text()
+        }
+        throw new Error('Network cdn response was not ok.')
+      })
+      .then(text => {
+        const script = document.createElement('script')
+        script.innerHTML = text.trim()
+        document.head.appendChild(script)
+      })
+      .catch(err => {
+        console.log('CDN load Error:', err)
+        return err
+      })
+  }
 
-      var div = document.createElement('div')
-      div.innerHTML = text.trim()
+  /**
+   * Returns Promise
+   */
+  const loadBanner = () => {
+    const uri = `${protocol}://${hostname}${portStr}/get`
+    fetch(uri)
+      .then(response => {
+        if(response.ok) {
+          return response.json()
+        }
+        throw new Error('Network response was not ok.')
+      })
+      .then(json => {
+        const { randomId, html } = json
+        let div = document.createElement('div')
+        div.innerHTML = html.trim()
+        const a = div.firstChild
+        const replace = document.querySelector('.some-banner-script')
+        replace.replaceWith(a)
+      })
+      .catch(err => {
+        console.log('Load banner error:', err)
+        return err
+      })
+  }
 
-      var a = div.firstChild
-      var replace = document.querySelector('.methads-script')
-      replace.replaceWith(a)
-    })
 
-    .catch(err => console.log('My Error:', err))
+  /**
+   * Main func
+   */
+  const doBanner = () => {
+    Promise.all([loadFingerprint2Cdn(), loadBanner()])
+      .then(async values => {
+        const result = await fingerprintHashDo()
+        console.log('Result=', result)
+      })
+  }
+
+
+  // Use 'requestIdleCallback' API if possible.
+  if ('requestIdleCallback' in window) {
+    console.log('requestIdleCallback is ON!')
+    requestIdleCallback(doBanner);
+  }
+  else {
+    console.log('requestIdleCallback is OFF!')
+    setTimeout(doBanner, 1)
+  }
 
 })()
