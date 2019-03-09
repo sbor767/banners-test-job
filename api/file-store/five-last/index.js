036cc672728e68879de60c611f40c1eba0341d53
Aleksandr Borkov <sbor767@yandex.ru>
2019-03-09 20:51:06 +0300

    Fix Promises logics in file-store.

--- api/file-store/five-last/index.js
+++ api/file-store/five-last/index.js
@@ -1,66 +1,76 @@
+/**
+ * Ensures the persistence of the last five client requests in a file by the queue model.
+ * Rejects other write requests while recording.
+ */
+
 const FILE_STORE_PATH = './public/file-store/client-info.json'
 const fs = require('fs')
 const util = require('util')
 
+/**
+ * Returns Promise
+ */
 const saveIfNotBusy = clientInfoObj => {
   let isBusy
 
   return () => {
-    console.log(`===============isBusy====${isBusy}==============`)
-    if (isBusy) return Promise.reject('File store is busy!')
+    if (isBusy) return Promise.reject('File storage is busy!')
 
     isBusy = true
     return read()
       .then(arr => {
-        let newArr = [...arr]
-        // Truncate if five or more.
-        if (newArr.length >= 5) newArr.length = 4
-        newArr.unshift(clientInfoObj)
-        return newArr
-      })
+          let newArr = [...arr]
+          // Truncate if five or more.
+          if (newArr.length >= 5) newArr.length = 4
+          newArr.unshift(clientInfoObj)
+          return newArr
+        },
+        err => {throw `File store write error when reading: ${err}`}
+      )
 
       .then(toWriteArr => {
-        const writeFile = util.promisify(fs.writeFile)
-        writeFile(FILE_STORE_PATH, JSON.stringify(toWriteArr, null, 4))
-          .then(code => {
+        return util.promisify(fs.writeFile)(FILE_STORE_PATH, JSON.stringify(toWriteArr, null, 4))
+          .then(() => {
             isBusy = false
-            return code
+            return 'ok'
           })
-          .catch(err => {
-            isBusy = false
-            throw err
-          })
-
       })
 
-      .catch(err => {throw `File store write error when reading: ${err}`})
+      .catch(err => {
+        isBusy = false
+        throw err
+      })
   }
 }
 
+/**
+ * Returns Promise
+ */
 const save = clientInfoObj => {
   return saveIfNotBusy(clientInfoObj)()
 }
 
+/**
+ * Returns Promise
+ */
 const read = () => {
   const readFile = util.promisify(fs.readFile)
   return readFile(FILE_STORE_PATH, 'utf8')
-    .then(data => {
-      let arr
-      try {
-        arr = JSON.parse(data)
-      } catch (e) {
-        throw `Json parse error when read file: ${e}`
+    .then(
+      JSON.parse,
+      err => {
+        // Check for ENOENT and return empty array if so.
+        if (err.code === 'ENOENT') return []
+        throw err
       }
+    )
+    .then(arr => {
       if (!Array.isArray(arr)) throw 'Data is not array when read file.'
       return arr
     })
     .catch(err => {
-      // Check for ENOENT and return empty array if so.
-      if (err.code === 'ENOENT') return []
-
       throw `Failed read file-store with error: ${err}`
     })
 }
 
 exports.save = save
-exports.read5last = read
